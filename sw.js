7605443429ddabe77492070c1217ed6f08b70b18
lukap13588-max <lukap13588@gmail.com>
2026-08-16 13:19:54 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='doctor-rush-v3';
-const ASSETS=['./','./index.html','./style.css','./game.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
